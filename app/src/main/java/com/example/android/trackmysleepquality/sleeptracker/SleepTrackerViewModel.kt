c57b0fb7b4b1156bd353e2219eecbcf52624ae54
animesh <royanimesh2211@gmail.com>
2022-11-13 10:52:07 +0530

    Update button state enable/disable

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -50,6 +50,16 @@ class SleepTrackerViewModel(
                 _navigateToSleepQuality.value = null
         }
 
+        val startButtonVisible = Transformations.map(tonight) {
+                it == null
+        }
+        val stopButtonVisible = Transformations.map(tonight) {
+                it != null
+        }
+        val clearButtonVisible = Transformations.map(nights) {
+                it?.isNotEmpty()
+        }
+
         private fun initializeTonight() {
                 viewModelScope.launch {
                         tonight.value = getTonightFromDatabase()
